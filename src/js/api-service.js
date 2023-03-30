a9359045d8994c2d77e8cbafdb73a2a623872195
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-03-30 23:18:24 +0300

    card id

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,8 +1,11 @@
 import initPagination from './pagination';
 import axios from 'axios';
 
-const API_URL_IMG=`https://image.tmdb.org/t/p/original`;
+
 const API_KEY = '34e68a416eb051ec4adf34df5a0038fd';
+const API_URL=`https://api.themoviedb.org/3/`;
+const API_URL_IMG=`https://image.tmdb.org/t/p/original`;
+
 
 
 const moviesAll = document.querySelector(".gallery");
@@ -11,14 +14,14 @@ getMovies(1);
 initPagination(1000, getMovies); 
 
 async function getMovies(page) {
-    const resp = await fetch( `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=${page}`);
+    const resp = await fetch( `${API_URL}/trending/all/day?api_key=${API_KEY}&page=${page}`);
 
     const respData = await resp.json();
   //  console.log(respData);
 
     const data = respData["results"].map(item => {
         return `
-            <li class="movie-card">
+            <li class="movie-card"  ID=${item.id}> >
                 <img class="movie-card__image" src="${API_URL_IMG}${item.poster_path}" 
                 onerror="this.onerror=null;this.src='https://thumbs.dreamstime.com/b/атрибуты-кино-вьюрок-фи-ьма-и-во-а-со-ы-в-бумажном-стаканчике-87336791.jpg'" 
                 alt="${item.original_title}" 
