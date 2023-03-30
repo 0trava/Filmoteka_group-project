36d4e21c35d3cb5bd98fb4ae4d772988f64bae29
0trava <102797527+0trava@users.noreply.github.com>
2023-03-30 17:03:18 +0000

    Deploying to gh-pages from @ 0trava/Filmoteka_group-project@710825a6b35929c0d93a704bf18864ecfb2bc1d7 🚀

--- index.3a82abc4.js
+++
@@ -1,2 +0,0 @@
-!async function(a){const o=await fetch(a,{headers:{"Content-Type":"application/json","X-API-KEY":"a183424c-2c54-4420-b62b-801b3a7db5c4"}}),t=await o.json();console.log(t)}("https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1");
-//# sourceMappingURL=index.3a82abc4.js.map
