e5294c5f93d89a3e7373c6897f33d82d16077f11
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-03-31 13:46:12 +0300

    card-click finish

--- src/js/card-click.js
+++ src/js/card-click.js
@@ -5,6 +5,24 @@
 //  ФІНІШ - запускаємо функцію куди передаємо ID картки ( для запукси модалки та рендеренгу). Для відпрацювання поки що передаємо у console.log
 
 
+let MOVIE_ID ="";
+
+// Підключаємось до данних сторінки index.html
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.addEventListener('click', getMovieId);
+
+
+function  getMovieId(evt) {
+    const getMovieID = evt.target.id; // Отримали ID картки на яку був клік
+    console.log(getMovieID); // !!!!!!!!!!!!!!!   ПІДСТАВИТИ ФУНКЦІЮ ДЛЯ ПЕРЕДАЧІ ID
+};
+
+
+
+
 
 
 
