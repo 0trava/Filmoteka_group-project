f9458a0a1b974ad09454b44ead36e5af6fde1321
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-03-31 10:07:31 +0300

    update

--- src/js/trailer-api.js
+++ src/js/trailer-api.js
@@ -5,11 +5,11 @@ const API_KEY = '3fae8df0f78070a007ffb073b1444710';
 getVideos(603692);
 
 async function getVideos(movieID) {
-  const resp = await fetch(
+  const response = await fetch(
     `${API}movie/${movieID}/videos?api_key=${API_KEY}&language=en-US`
   );
 
-  const respData = await resp.json();
+  const responseData = await response.json();
 
-  console.log(respData);
+  console.log(responseData);
 }
