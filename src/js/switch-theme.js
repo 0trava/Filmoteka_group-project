3150730f2fef1bdfdecca34bcc4cb8e53cec696a
Demag0g1 <konievanatol@gmail.com>
2023-04-01 16:22:53 +0300

    switch-theme
    Повністю змінив логіку
    
    Залишилась тільки інверсія на пагінації

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,110 +1,51 @@
-const switcher = document.getElementById('switcher');
-const link = document.getElementById('theme-link');
-
-const savedTheme = localStorage.getItem('currTheme');
-
-document.addEventListener('DOMContentLoaded', () => {
-  const currentTheme = loadTheme('currTheme');
-  setTheme(currentTheme);
-});
-
-switcher.addEventListener('click', () => {
-  changeTheme();
-});
-
-const loadTheme = currTheme => {
-  try {
-    let theme = localStorage.getItem(currTheme);
-
-    if (theme === null) {
-      theme = 'light';
-      saveTheme(currTheme, theme);
-    }
-    return theme;
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+  GREY: 'grey-background-theme',
 };
 
-const saveTheme = (currTheme, theme) => {
-  try {
-    localStorage.setItem(currTheme, theme);
-  } catch (error) {
-    console.error('Set state error: ', error.message);
-  }
-};
-
-const setTheme = currentTheme => {
-  const { lightTheme, darkTheme, lightBackground, darkBackground } =
-    getThemeStyles();
-
-  if (currentTheme === 'dark') {
-    switcher.checked = true;
-    link.setAttribute('href', darkTheme);
-    document.body.style.backgroundColor = darkBackground;
-    setElementsColor('#000');
-  } else {
-    switcher.checked = false;
-    link.setAttribute('href', lightTheme);
-    document.body.style.backgroundColor = lightBackground;
-    setElementsColor('#fff');
-  }
-};
-
-const changeTheme = () => {
-  const { lightTheme, darkTheme, lightBackground, darkBackground } =
-    getThemeStyles();
-  let theme = 'light';
-  setElementsColor('#ffffff');
-  let currTheme = link.getAttribute('href');
-
-  if (currTheme === lightTheme) {
-    currTheme = darkTheme;
-    theme = 'dark';
-    saveTheme('currTheme', theme);
-    document.body.style.backgroundColor = darkBackground;
-    setElementsColor('#ffffff');
-  } else {
-    currTheme = lightTheme;
-    theme = 'light';
-    saveTheme('currTheme', theme);
-    document.body.style.backgroundColor = lightBackground;
-    setElementsColor('#000000');
-
-  }
-
-  link.setAttribute('href', currTheme);
-};
-
-const getThemeStyles = () => {
-  const lightTheme = '/src/sass/components/_switch-light-btn.scss';
-  const darkTheme = '/src/sass/components/_switch-dark-btn.scss';
-  const lightBackground = '#d8dbe0';
-  const darkBackground = '#28292c';
-  const lightFont = '#000000';
-  const darkFont = '#ffffff';
-  return {
-    lightTheme,
-    darkTheme,
-    lightBackground,
-    darkBackground,
-    lightFont,
-    darkFont,
-  };
-};
+const body = document.querySelector('body');
+const footer = document.querySelector('footer');
+const modal = document.querySelector('.modal');
+// const card = document.querySelector('.movie-card__name');
 
 const setElementsColor = color => {
-  const elements = document.querySelectorAll(
-    '.movie-card__name',
-    '.tui-page-btn',
-    '.home-footer-page'
-  );
+  const elements = document.querySelectorAll('.movie-card__name');
   for (let i = 0; i < elements.length; i++) {
     elements[i].style.color = color;
   }
 };
 
-export default {
-  saveTheme,
-  loadTheme,
+const delClassElem = () => {
+  body.classList.remove(Theme.LIGHT, Theme.DARK);
+  footer.classList.remove(Theme.LIGHT, Theme.GREY);
+  modal.classList.remove(Theme.LIGHT, Theme.GREY);
 };
+
+const setTheme = theme => {
+  if (theme === 'darkTheme') {
+    body.classList.add(Theme.DARK);
+    footer.classList.add(Theme.GREY);
+    modal.classList.add(Theme.GREY);
+    setElementsColor('#ffffff');
+  } else {
+    body.classList.add(Theme.LIGHT);
+    modal.classList.add(Theme.LIGHT);
+    setElementsColor('#121213f7');
+  }
+};
+
+const themeSwitcher = document.querySelector('#switcher');
+
+themeSwitcher.addEventListener('change', () => {
+  const theme = themeSwitcher.checked ? 'darkTheme' : 'lightTheme';
+  localStorage.setItem('Theme', theme);
+  delClassElem();
+  setTheme(theme);
+});
+
+const savedTheme = localStorage.getItem('Theme');
+if (savedTheme !== null) {
+  themeSwitcher.checked = savedTheme === 'darkTheme';
+  setTheme(savedTheme);
+}
