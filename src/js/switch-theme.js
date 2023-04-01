78d5ed881a636ebf6176785604f3c6532e2def88
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-04-01 19:29:43 +0300

    fix modal

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -6,7 +6,7 @@ const Theme = {
 
 const body = document.querySelector('body');
 const footer = document.querySelector('footer');
-const modal = document.querySelector('.modal');
+// const modal = document.querySelector('.modal');
 // const card = document.querySelector('.movie-card__name');
 
 const setElementsColor = color => {
@@ -19,18 +19,18 @@ const setElementsColor = color => {
 const delClassElem = () => {
   body.classList.remove(Theme.LIGHT, Theme.DARK);
   footer.classList.remove(Theme.LIGHT, Theme.GREY);
-  modal.classList.remove(Theme.LIGHT, Theme.GREY);
+  // modal.classList.remove(Theme.LIGHT, Theme.GREY);
 };
 
 const setTheme = theme => {
   if (theme === 'darkTheme') {
     body.classList.add(Theme.DARK);
     footer.classList.add(Theme.GREY);
-    modal.classList.add(Theme.GREY);
+    // modal.classList.add(Theme.GREY);
     setElementsColor('#ffffff');
   } else {
     body.classList.add(Theme.LIGHT);
-    modal.classList.add(Theme.LIGHT);
+    // modal.classList.add(Theme.LIGHT);
     setElementsColor('#121213f7');
   }
 };
