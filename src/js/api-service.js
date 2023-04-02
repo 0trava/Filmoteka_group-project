2109f2c69256e36eb1b580b19c82470e53af1755
Olha Lushina <olga.lushina.ua@gmail.com>
2023-04-02 04:19:33 +0300

    add movie generes

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -129,3 +129,47 @@ export async function getSearchMovies(query, page) {
   
   return respDataSearch.total_results;
 }
+
+// Функція виводу фільмів за жанром
+export async function getMoviesByGenereId(genere_id, page) {
+    // Включити loader
+    const spinner = document.querySelector('.dot-spinner');
+    spinner.classList.remove('is-hidden');
+
+    // Отримати дані з сервера
+    const resp = await fetch( `${API_URL}discover/movie?api_key=${API_KEY}&page=${page}&with_genres=${genere_id}`);
+
+    const respData = await resp.json();
+
+    // Виключити loader
+    spinner.classList.add('is-hidden');
+
+    // Сформувати карточки фільмів
+    const data = respData['results']
+      .map(({ id, poster_path, title, genre_ids, release_date }) => {
+        let genre = getGenre(genre_ids);
+        let year = release_date?.substring(0, 4);
+        //   if (genre && year) genre += ' | ';
+          if (title) {
+                    return  `
+                    <li class="movie-card"  ID=${id}>
+                        <img class="movie-card__image" src="${API_URL_IMG}${poster_path}" 
+                        onerror="this.onerror=null;this.src='https://thumbs.dreamstime.com/b/атрибуты-кино-вьюрок-фи-ьма-и-во-а-со-ы-в-бумажном-стаканчике-87336791.jpg'" 
+                        alt="${title}" 
+                        width="300" ID=${id}>
+                        <h2 class="movie-card__name" ID=${id}>${title}</h2>
+                        <p class="movie-card__text" ID=${id}>${genre} | ${year}</p>
+        
+                    </li>
+                `;
+          };
+
+
+      })
+      .join('');
+  
+  // Додати фільми у галерею
+  moviesAll.innerHTML = data;
+  
+  return respData.total_results;
+}
